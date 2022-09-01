2ebcdd3ed6df42be33653d0202c61ea6795142c1
Leo Nguyen <lichnguyenz@gmail.com>
2022-09-01 17:26:18 -0500

    Update hw1.py

--- Assignments/Homework1/hw1.py
+++ Assignments/Homework1/hw1.py
@@ -1,8 +1,9 @@
 # Homework1: Text Processing with Python
 
- 
-import sys  # to get the system parameter
-import os # to process the filepath
+
+import sys
+import os
+import re
 
 
 class Person:
@@ -23,33 +24,61 @@ class Person:
 def text_processing(raw_text):
     lines = raw_text.splitlines(True)
     data = [lines[i] for i in range(1, len(lines))]
-    employee_list = []
+    employee_list = {}  # create a dict of employees
     for person in data:
         last, first, mi, id, phone = person.split(',')
-        employee = Person(last, first, mi, id, phone)
-        employee_list.append(employee)
 
-    print('\nTest text_processing: \n')
-    for e in employee_list:
-        e.display()
+        last = last.capitalize()
+
+        first = first.capitalize()
+
+        if mi != '':
+            mi = mi.capitalize()
+        else:
+            mi = 'X'
+
+        while True:
+            if len(id) == 6 and id[:2].isalpha() and id[2:].isnumeric():
+                break
+            else:
+                print('ID invalid: ', id)
+                print('ID is two letters followed by 4 digits')
+                id = input('Please enter a valid id: ')
+                id = id[:2].upper() + id[2:]  # auto convert first 2 characters to upper case
+
+        ## might need to work on force user to input correct phone format
+
+        phone = re.sub(r'\D', '', phone)  # get rid of non-digit chars
+        assert (len(phone) == 10)
+        phone = f"{phone[:3]}-{phone[3:6]}-{phone[6:]}"
+
+        employee = Person(last, first, mi, id, phone)
+        id_list = employee_list.keys()
+        # print('ID List: ', id_list)  #debug
+        if id in id_list:
+            print('ID:', id, 'is duplicate')
+        else:
+            employee_list.update({id: employee})
+    return employee_list
 
 
 def main():
     # Method to handle the filepath in both Window and Mac
-    def method1(filepath):
-        print("\nUsing method 1")
-
+    def read_file(filepath):
         with open(os.path.join(os.getcwd(), filepath), 'r') as f:
-            text_in = f.read()
-        print(text_in)
-        text_processing(text_in)
+            fin= f.read()
+        # print(text_in) #debug
+        return fin
 
     if len(sys.argv) < 2:
         print('Please enter a filename as a system arg')
     else:
         fp = sys.argv[1]
-        method1(fp)
+        fin= read_file(fp)
+        employees = text_processing(fin)
 
+        for e in employees.values():
+            e.display()
 
 if __name__ == "__main__":
     main()
