0e6a809a1764264def0886ef5d0a248cb1163e65
Leo Nguyen <lichnguyenz@gmail.com>
2022-11-09 00:22:13 -0600

    Add user list

--- Assignments/Chatbot Project/chatbot.py
+++ Assignments/Chatbot Project/chatbot.py
@@ -7,6 +7,7 @@ import random
 
 
 def related(x_text):
+    x_text = x_text.lower()
     if "name" in x_text:
         y_text = "what's your name?"
     elif "weather" in x_text:
@@ -64,20 +65,49 @@ def send_message(message, user_name):
 # main
 def main():
     user_name = input("Please enter your username:")
+    user_name = user_name.lower()
+
+    # key = user_name
+    # value = [name, personal info, like, dislike, personal remarks]
+
+    # personal remarks: top 20 word in the whole conversation (Current and Past, each for each text)
+
+    ## like/dislike: tokens, sentences --> anything after that word
+    dict_username = {}
 
     print('\n************\n')
     bot_template = "Bot : {0}"
 
-    greet1 = "Hi " + user_name + "!. " + "Good to see you again"
-    print(bot_template.format(greet1))
+    exist_user_greeting = "Hi " + user_name + "!. " + "Good to see you again"
+    new_user_greeting = "Hi there! What should i call you?"
 
-    while True:
-        user_input = input(user_name + ' : ')
-        user_input = user_input.lower()
-        related_text = related(user_input)
-        send_message(related_text, user_name)
-        if user_input == "exit" or user_input == "stop":
-            break
+    if user_name in dict_username:
+        print(bot_template.format(exist_user_greeting))
+        # open pickle
+    # get everything ready for conservation
+    else:  # New user. Build new profile
+        print(bot_template.format(new_user_greeting))
+        name = input('User : ').lower()
+        dict_username[user_name] = [name]
+
+        # dict_username[user_name].append('123')
+        # print('Name of user:' + dict_username[user_name][1])  # debug
+
+        while True:
+            user_input = input(name.title() + ' : ')
+            if user_input == "exit" or user_input == "stop":
+                break
+            related_text = related(user_input)
+            send_message(related_text, user_name)
+
+
+    # while True:
+    #     user_input = input(user_name + ' : ')
+    #     user_input = user_input.lower()
+    #     related_text = related(user_input)
+    #     send_message(related_text, user_name)
+    #     if user_input == "exit" or user_input == "stop":
+    #         break
 
 
 if __name__ == "__main__":
